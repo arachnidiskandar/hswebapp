36751101051ba4cc9d62062eb23126d6f2037d32
Augusto Santos <augstarachrafael@gmail.com>
2019-09-04 17:12:02 -0300

    remoção componentes do module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProdutoComponent } from './components/produto/produto.component';
 import { ProdutosComponent } from './components/produtos/produtos.component';
 import { FilterPipe } from './pipes/filtroProdutos/filter.pipe';
-import { KitupgradeComponent } from './components/categorias/kitupgrade/kitupgrade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProdutoComponent,
     ProdutosComponent,
-    FilterPipe,
-    KitupgradeComponent
+    FilterPipe
   ],
   imports: [
     BrowserModule,
